9965fe30d4a374657aa8dbaa42190fd0f417b952
Manh Nam, Tran <manhnambeupu@gmail.com>
2024-07-07 00:16:08 +0200

    delete the wrong interface

--- API_web_shop/src/main/java/web_shop/api_web_shop/repository/SanPhamChiTiet.java
+++
@@ -1,6 +0,0 @@
-package web_shop.api_web_shop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SanPhamChiTiet extends JpaRepository<SanPhamChiTiet, Long> {
-}
